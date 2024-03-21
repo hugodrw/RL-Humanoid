83c2db058a5c4acd1a3480794268489d88d0ab90
apoupon <81863563+apoupon@users.noreply.github.com>
2024-03-21 11:07:17 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ### Benchmarking RL methods using Gym and Stable-Baselines
+
+Spinningup 
+https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/ppo/core.py
+
+Stable-baselines
+https://github.com/DLR-RM/stable-baselines3/blob/master/stable_baselines3/ppo/ppo.py
+
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,22 @@
 import numpy as np
-import scipy.signal
-from gym.spaces import Box, Discrete
+#import scipy.signal
+from gym.spaces import Box
 
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
-from torch.distributions.categorical import Categorical
-
 
 def combined_shape(length, shape=None):
     pass
 
 
+# on laisse la possibilité de moduler le NN ou on en code un immutable dans l'actor et le critic?
 def mlp(sizes, activation, output_activation=nn.Identity):
-    pass
-
+    layers = []
+    for j in range(len(sizes)-1):
+        act = activation if j < len(sizes)-2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
+    return nn.Sequential(*layers)
 
 def count_vars(module):
     pass
@@ -36,45 +38,31 @@ def discount_cumsum(x, discount):
     """
     pass
 
-class Actor(nn.Module):
+
+class MLPGaussianActor(nn.Module):
+
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        std = 0.5 * np.ones(act_dim, dtype=np.float32)  # might change: it's possible to have a dynamic std to better explore (https://kae1506.medium.com/actor-critic-methods-with-continous-action-spaces-having-too-many-things-to-do-e4ff69cd537d)
+        self.std = torch.nn.Parameter(torch.as_tensor(std)) 
+        self.mu_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
 
     def _distribution(self, obs):
-        raise NotImplementedError
+        mu = self.mu_net(obs)
+        return Normal(mu, self.std)
 
     def _log_prob_from_distribution(self, pi, act):
-        raise NotImplementedError
+        return pi.log_prob(act).sum(axis=-1)
 
     def forward(self, obs, act=None):
         # Produce action distributions for given observations, and 
         # optionally compute the log likelihood of given actions under
         # those distributions.
-        pass
-
-
-class MLPCategoricalActor(Actor):
-    
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
-        super().__init__()
-        pass
-
-    def _distribution(self, obs):
-        pass
-
-    def _log_prob_from_distribution(self, pi, act):
-        pass
-
-
-class MLPGaussianActor(Actor):
-
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
-        super().__init__()
-        pass
-
-    def _distribution(self, obs):
-        pass
-
-    def _log_prob_from_distribution(self, pi, act):
-        pass
+        pi = self._distribution(obs)
+        logp_a = None
+        if act is not None:
+            logp_a = self._log_prob_from_distribution(pi, act) # OK for TRPO but is it used in PPO ? (cf Loss TRPO vs PPO)
+        return pi, logp_a
 
 class MLPCritic(nn.Module):
 
@@ -86,7 +74,6 @@ class MLPCritic(nn.Module):
 
 
 class MLPActorCritic(nn.Module):
-
 
     def __init__(self, observation_space, action_space, 
                  hidden_sizes=(64,64), activation=nn.Tanh):
