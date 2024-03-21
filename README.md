839f7337adb46e4fb79293b9955dbd6a24912224
apoupon <81863563+apoupon@users.noreply.github.com>
2024-03-21 16:41:08 +0100

    update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ### Benchmarking RL methods using Gym and Stable-Baselines
 
-Spinningup 
+Spinning up : 
 https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/ppo/core.py
 
-Stable-baselines
+Stable-baselines : 
 https://github.com/DLR-RM/stable-baselines3/blob/master/stable_baselines3/ppo/ppo.py
 
+Implem details : 
+https://iclr-blog-track.github.io/2022/03/25/ppo-implementation-details/#Schulman2015
--- src/utils.py
+++ src/utils.py
@@ -43,15 +43,26 @@ class MLPGaussianActor(nn.Module):
 
     def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
         super().__init__()
-        std = 0.5 * np.ones(act_dim, dtype=np.float32)  # might change: it's possible to have a dynamic std to better explore (https://kae1506.medium.com/actor-critic-methods-with-continous-action-spaces-having-too-many-things-to-do-e4ff69cd537d)
-        self.std = torch.nn.Parameter(torch.as_tensor(std)) 
+        # we initialize log_std and make it learnable
+        # the std is used in the normal distribution used by the policy to control exploration
+        # we learn log_std instead of std because 
+        #   1. std must be positive whereas log_std is unbounded as the exponential will make it positive (which simplifies the optimization process)
+        #   2. log space is generally more numerically stable
+        #   3. log standard deviation parameter can be adjusted more finely than the standard deviation to have a better control over exploration behavior of the policy
+        log_std = -0.5 * np.ones(act_dim, dtype=np.float32)
+        self.log_std = torch.nn.Parameter(torch.as_tensor(log_std))
+
+        # mapping from state to best action (batch of states to batch of actions ?)
         self.mu_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
 
     def _distribution(self, obs):
+        # Normal distribution used by the policy to control exploration
         mu = self.mu_net(obs)
-        return Normal(mu, self.std)
+        std = torch.exp(torch.tensor(self.log_std))
+        return Normal(mu, std)
 
     def _log_prob_from_distribution(self, pi, act):
+        # log prob, not sure when it is used (in TRPO Loss-> OK, in PPO loss -> ??)
         return pi.log_prob(act).sum(axis=-1)
 
     def forward(self, obs, act=None):
@@ -61,16 +72,17 @@ class MLPGaussianActor(nn.Module):
         pi = self._distribution(obs)
         logp_a = None
         if act is not None:
-            logp_a = self._log_prob_from_distribution(pi, act) # OK for TRPO but is it used in PPO ? (cf Loss TRPO vs PPO)
-        return pi, logp_a
+            logp_a = self._log_prob_from_distribution(pi, act) 
+        return pi, logp_a 
 
 class MLPCritic(nn.Module):
 
     def __init__(self, obs_dim, hidden_sizes, activation):
         super().__init__()
-        pass
+        self.v_net = mlp([obs_dim] + list(hidden_sizes) + [1], activation)
+    
     def forward(self, obs):
-        pass
+        return self.v_net(obs).squeeze(-1) # squeeze to remove last dim -> critical to ensure v has right shape
 
 
 class MLPActorCritic(nn.Module):
@@ -78,11 +90,19 @@ class MLPActorCritic(nn.Module):
     def __init__(self, observation_space, action_space, 
                  hidden_sizes=(64,64), activation=nn.Tanh):
         super().__init__()
-        pass
+
+        obs_dim = observation_space.shape[0]
+
+        self.pi = MLPGaussianActor(obs_dim, action_space.shape[0], hidden_sizes, activation)
+        self.v = MLPCritic(obs_dim, hidden_sizes, activation)
 
     def step(self, obs):
         with torch.no_grad():
-            pass
+            pi = self.pi._distribution(obs)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v = self.v(a)
+            return a.numpy(), logp_a.numpy(), v.numpy() # why numpy instead of tensor? 
 
     def act(self, obs):
-        pass
+        return self.step(obs)[0]
