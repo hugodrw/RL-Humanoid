1b230516ea73c4239840556ad833c8417bddc92b
hugodrw <hugoderohanwillner@gmail.com>
2024-04-08 18:28:23 +0200

    final commit

--- src/ppo.py
+++ src/ppo.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--cpu', type=int, default=1)
     parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=50000)
     parser.add_argument('--exp_name', type=str, default='ppo')
     parser.add_argument('--normalize_env', type=bool, default='False')
 
